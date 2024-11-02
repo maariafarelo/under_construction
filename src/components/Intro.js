61f7278c59d01032dcc22e26efeeee216e9e59ff
maariafarelo <maria190504@gmail.com>
2024-11-02 17:48:14 +0100

    Does what i want but not with good practise. Provisional image,

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,19 +1,26 @@
 import React from "react";
+import profileImage from './logo.png'; 
 
 function Intro() {
   return (
-    <div className="intro">
-      <h2 className="intro-title">
-        hi, <span className="highlighted-name">Maria</span> here!
-      </h2>
-      <p className="intro-description">Just a creative soul exploring digital horizons.</p>
-      <p className="centered-text">
-        I'm an Informatics Engineering student based in Barcelona, 
-        passionate about learning and an avid hackathon enthusiast.
-        I enjoy organizing events that foster innovation and 
-        collaboration, and I thrive on the excitement of developing 
-        creative solutions under pressure.
-      </p>
+    <div className="intro-container">
+      <div className="intro">
+        <h2 className="intro-title">
+          hi, <span className="highlighted-name">Maria</span> here!
+        </h2>
+        <p className="intro-description">Just a creative soul exploring digital horizons.</p>
+        <p className="centered-text">
+          I'm an Informatics Engineering student based in Barcelona, 
+          passionate about learning and an avid hackathon enthusiast.
+          I enjoy organizing events that foster innovation and 
+          collaboration, and I thrive on the excitement of developing 
+          creative solutions under pressure.
+        </p>
+        <a href="mailto:maria190504@gmail.com" className="contact-button">
+          Say hi!
+        </a>
+      </div>
+      <img src={profileImage} alt="Maria" className="profile-image" /> {/* Image element */}
     </div>
   );
 }
