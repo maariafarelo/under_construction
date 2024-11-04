b56971dcb2b4623be1813fd01e90195c4aad318c
maariafarelo <maria190504@gmail.com>
2024-11-04 19:06:40 +0100

    4/11/2024

--- src/index.js
+++ src/index.js
@@ -17,6 +17,20 @@ document.addEventListener('mousemove', (e) => {
   cursor.style.top = `${e.pageY}px`;
 });
 
+document.querySelectorAll('.contact-button, .cv-button').forEach(button => {
+  button.addEventListener('mousedown', () => {
+    button.classList.add('clicked'); // Add class when button is clicked
+  });
+
+  button.addEventListener('mouseup', () => {
+    button.classList.remove('clicked'); // Remove class when mouse is released
+  });
+
+  button.addEventListener('mouseleave', () => {
+    button.classList.remove('clicked'); // Remove class if mouse leaves the button area
+  });
+});
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
