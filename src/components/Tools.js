3f1bc2b7fdf551b7e8f87b4d893a9a02480bf1ea
maariafarelo <maria190504@gmail.com>
2024-11-05 16:11:27 +0100

    estructura feta, cal canviar per images

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -7,23 +7,31 @@ function Tools() {
       <h2 className="tools-title">my tools</h2>
       <div className="tools">
         <div className="column">
-          <h3 className="column-title">Languages & Frameworks</h3>
+
+          <h3 className="column-title">Languages</h3>
           <ul>
             <p>JavaScript</p>
+            <p>Java</p>
             <p>Python</p>
+            <p>C++</p>
+            <p>Dart</p>
+            <p>CSS & HTML</p>
+          </ul>
+
+          <h3 className="column-title">Frameworks</h3>
+          <ul>
+            <p>Flutter</p>
             <p>React</p>
-            <p>Node.js</p>
-            <p>CSS</p>
           </ul>
         </div>
+
         <div className="column">
           <h3 className="column-title">Tools</h3>
           <ul>
             <p>Git</p>
-            <p>Docker</p>
+            <p>JetBrains tools</p>
             <p>VS Code</p>
-            <p>Postman</p>
-            <p>Webpack</p>
+            <p> Microsoft Tools </p>
           </ul>
         </div>
         <div className="column">
@@ -31,8 +39,8 @@ function Tools() {
           <ul>
             <p>Figma</p>
             <p>Slack</p>
-            <p>Trello</p>
-            <p>Zoom</p>
+            <p>Netlify</p>
+            <p>Meet</p>
             <p>Notion</p>
           </ul>
         </div>
