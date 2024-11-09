31609ec5d7af36c8677215d722c1cf65b39b3f2f
maariafarelo <maria190504@gmail.com>
2024-11-09 19:47:32 +0100

    title

---
+++ src/components/PetProjects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './PetProjects.css';
+
+function Tools() {
+  return (
+    <div className="PetProjects-container">
+      <h2 className="PetProjects-title">some of my pet projects</h2>
+
+      <div className="PetProjects">
+      </div>
+    </div>
+  );
+}
+
+export default Tools;
